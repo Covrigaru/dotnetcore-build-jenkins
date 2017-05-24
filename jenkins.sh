56d1e5ecbb06de2f53082aa4bb583fe9c9a42e50
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2017-05-24 10:58:29 -0400

    Add nginx.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,10 @@
 # Install using deb packages:
-```
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins -y
-```
+
 # Install required plug-ins:
-```
 JENKINS_URL="http://127.0.0.1:8080/"
 USERNAME="admin"
 PASSWORD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
@@ -16,4 +14,5 @@ java -jar jenkins-cli.jar -s $JENKINS_URL install-plugin dashboard-view --userna
 java -jar jenkins-cli.jar -s $JENKINS_URL install-plugin bitbucket --username "$USERNAME" --password "$PASSWORD"
 # Required for using Aws Credentials
 java -jar jenkins-cli.jar -s $JENKINS_URL install-plugin aws-credentials --username "$USERNAME" --password "$PASSWORD"
-```
+
+sudo service jenkins restart
---
+++ nginx.sh
@@ -0,0 +1,47 @@
+# NginX reverse proxy for jenkins: https://gist.github.com/rdegges/913102
+FQDN="jenkins.end-points.io""
+
+sudo apt install nginx -y
+sudo mkdir /etc/nginx/certs
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/key.key -out /etc/nginx/certs/cert.crt -subj "/C=CA/ST=Ontario/L=Concord/O=end-points.io/OU=DevOps/CN=$FQDN"
+
+sudo rm /etc/nginx/sites-available/default
+
+echo "
+upstream jenkins {
+  server 127.0.0.1:8080 fail_timeout=0;
+}
+
+server {
+  listen 80 default;
+  server_name 127.0.0.1 $FQDN;
+  rewrite ^ https://$server_name$request_uri? permanent;
+}
+
+server {
+  listen 443 default ssl;
+  server_name 127.0.0.1 $FQDN;
+
+  ssl_certificate           /etc/nginx/certs/cert.crt;
+  ssl_certificate_key       /etc/nginx/private/key.key;
+
+  ssl_session_timeout  5m;
+  ssl_protocols  SSLv3 TLSv1;
+  ssl_ciphers HIGH:!ADH:!MD5;
+  ssl_prefer_server_ciphers on;
+
+  location / {
+    proxy_set_header Host $http_host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-Proto https;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_redirect http:// https://;
+
+    add_header Pragma \"no-cache\";
+
+    proxy_pass http://jenkins;
+  }
+}" | sudo tee /etc/nginx/sites-available/jenkins
+
+sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
+sudo service nginx restart
